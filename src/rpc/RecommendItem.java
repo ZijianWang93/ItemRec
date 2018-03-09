bce93c989aee9dea3838a2ddc0c9aa438b68e30e
ZijianWang93 <31352358+ZijianWang93@users.noreply.github.com>
2018-03-08 21:25:15 -0800

    Update RecommendItem.java

--- src/rpc/RecommendItem.java
+++ src/rpc/RecommendItem.java
@@ -22,19 +22,19 @@ import entity.Item;
 public class RecommendItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RecommendItem() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+    	/**
+    	 * @see HttpServlet#HttpServlet()
+    	 */
+    	public RecommendItem() {
+            super();
+            // TODO Auto-generated constructor stub
+    	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String userId = request.getParameter("user_id");
+	    String userId = request.getParameter("user_id");
 	    double lat = Double.parseDouble(request.getParameter("lat"));
 	    double lon = Double.parseDouble(request.getParameter("lon"));
 	    Recommendation recommendation = new GeoRecommendation();
@@ -50,8 +50,8 @@ public class RecommendItem extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	    // TODO Auto-generated method stub
+	    doGet(request, response);
 	}
 
 }
